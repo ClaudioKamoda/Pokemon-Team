1f8a01a3b1bd8f5d9e03f0294f04ad19606bd7a8
Claudio <claudiojlf@gmail.com>
2021-08-15 19:41:54 -0300

    started api logic

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
-const pokemonContainer = document.getElementsByClassName('pokemon-container')[0]
-const plusContainer = document.getElementsByClassName('plus-container')[0]
-const inputContainer = document.getElementsByClassName('input-container')[0]
+const buttonGetTeam = document.getElementById('getTeam')
+let basePokemonNumbers = [898, 898, 898, 898, 898, 898]
+let drawedNumbers = []
+let teamToShow
 
-const plus = document.getElementById('plus-sign')
+const teamContainer = document.getElementById('team-container')
 
-plusContainer.plus.addEventListener('click', () => {
-	plusContainer.classList.add('hide')
-	inputContainer.classList.remove('hide')
+buttonGetTeam.addEventListener('click', () => {
+	getNewTeam()
+
+	//send requests
+
+	//show the data on screen
+
+	console.log(teamToShow)
+	drawedNumbers = []
 })
+
+function makeRequest(pokemonToRequest){
+	axios
+		.get('https://pokeapi.co/api/v2/pokemon/1')
+		.then(function (response) {
+			// handle success
+			console.log(response)
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error)
+		})
+		.then(function () {
+			// always executed
+		})
+}
+
+
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * (max - 1)) + 1
+}
+
+function alreadyDrawed(element, array) {
+	return array.find(el => el == element) > 0 ? true : false
+}
+
+function getNewTeam() {
+	teamToShow = basePokemonNumbers.map(number => {
+		let aux = getRandomNumber(number)
+
+		while (alreadyDrawed(aux, drawedNumbers)) {
+			aux = getRandomNumber(number)
+		}
+		drawedNumbers.push(aux)
+
+		return aux
+	})
+}
